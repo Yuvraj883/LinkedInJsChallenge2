c7fc30a9cd4a418f13a032b5056ed99dba215af2
Yuvraj <kingyuvi88@gmail.com>
2022-08-26 15:45:36 +0530

    Pencil working

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.getElementById("canvas"); 
+const ctx = canvas.getContext('2d'); 
+let initX; 
+let initY; 
+let finX; 
+let finY; 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// ctx.fillStyle = "green"; 
+// ctx.fillRect(10,10,100,200); 
+// canvas.addEventListener("mousemove", (e)=>{
+// // ctx.beginPath(); 
+// // ctx.moveTo(e.clientX, e.clientY); 
+// ctx.fillStyle="red"; 
+// ctx.fillRect(e.clientX, e.clientY,2,2);
+// console.log("Mouse is moving!!!")
+// })
 
-setupCounter(document.querySelector('#counter'))
+canvas.addEventListener("mousedown",(e)=>{
+initX=e.clientX; 
+initY=e.clientY; 
+console.log(initX, initY); 
+})
+
+canvas.addEventListener("mousemove",(e)=>{
+  initX= finX; 
+  initY = finY; 
+  finX= e.clientX-canvas.getBoundingClientRect().left; 
+  finY= e.clientY-canvas.getBoundingClientRect().top; 
+
+  ctx.beginPath(); 
+  ctx.moveTo(initX, initY); 
+  ctx.lineTo(finX,finY); 
+  ctx.strokeWidth=2;
+  ctx.strokeStyle="red";
+  ctx.stroke();
+  ctx.closePath();
+ // console.log(finX, finY);
+
+
+})
